31b6605c9576be3a245f4d2012adfd73bb5b13d6
Jesus's_coder <136695751+Beastcoderdev@users.noreply.github.com>
2024-08-05 00:00:31 +0300

    v 1.0

--- main.dart
+++ main.dart
@@ -3,12 +3,14 @@ import 'dart:io';
 import 'Classes.dart';
 
 void main(){
+  
   //Loop the programe
+  Bankaccount p1 =Bankaccount(); // empty object before while loop so we can check if it is null or not
+
   while(true){
-    //Heading of the programe
+    if(p1.name == null && p1.balance == null){//Heading of the programe
     print('made by Antony');
     print('\n\n-----------------------Bank------------------------\n');
-
 
     // account register //
     stdout.write('your name: ');
@@ -20,7 +22,9 @@ void main(){
 
 
     // take a template from class
-    Bankaccount p1 =Bankaccount(name: Firstname[0], balance: balance);
+    p1 =Bankaccount(name: Firstname[0], balance: balance);
+    }
+    
 
     print('\n-----------Welcome to the Bank-----------\n'); 
 
@@ -28,11 +32,7 @@ void main(){
     // 
     print("Hello, ${p1.name}");
     print("your balance is ${p1.balance}");
-
-    
-    
-    // 
-
+      
 
 
     //menu of operations   
@@ -61,7 +61,7 @@ void main(){
 
     switch(operation){
       case 1:
-        print('\n********deposite operation********n'); 
+        print('\n********deposite operation********'); 
 
         stdout.write('\n\nEnter the amount: ');
         double amount = double.tryParse(stdin.readLineSync()!) ?? 0;
@@ -69,30 +69,33 @@ void main(){
         print("Done, your balance is: ${p1.deposite(amount)}");
        
       case 2:
-        print('\n********Withdraw operation********n'); 
+        print('\n********Withdraw operation********'); 
 
-        stdout.write('\n\nEnter the amount: ');
+        stdout.write('\nEnter the amount: ');
         double amount = double.tryParse(stdin.readLineSync()!) ?? 0;
         
         print("Done, your balance is: ${p1.withdraw(amount)}");
 
       case 3:
-        print('\n********Check balance operation********n'); 
+        print('\n********Check balance operation********'); 
         
         print("your balance is: ${p1.checkBalance()}");
 
       case 4:
-        print('\n********Transfer operation********n'); 
+        print('\n********Transfer operation********'); 
         stdout.write('\n\nEnter the account: ');
         String Account_other = stdin.readLineSync() ?? '';
+        List<String> Firstname_ao = Account_other.split(' ');
 
 
         stdout.write('\n\nEnter the amount: ');
         double amount = double.tryParse(stdin.readLineSync()!) ?? 0;
-      
+        // the money transfered to. account
+        Bankaccount p2 = Bankaccount(name: Firstname_ao[0], balance: amount); 
 
         print("Done, your balance: ${p1.transferMoney(Account_other, amount)}");
-
+        print("your transfered ${p2.balance} to ${p2.name}");
+        
       case 0:
         print("\nThis programe was written by Antony Hany \nI love flutter 😜");
         break;
@@ -106,13 +109,14 @@ void main(){
       print('Do u want to make another operations [ yes / no ]?');
       String? res = stdin.readLineSync() ?? "Sorry we didn't understand your input";
       
-      if(res =='yes'){
+      if(res.toLowerCase() =='yes'){
         print('*****************Restarting the programe*****************');    
       }else{
         print("Thank you for using our program");
         break;
       }
+    } 
 
-  }
+  
     
 } // end of main method
